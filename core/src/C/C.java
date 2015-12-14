046b1d3b9fb7553b4e000447e7cd9284822532d5
Your Name <you@example.com>
2015-12-15 06:38:57 +0800

    final commit

---
+++ core/src/C/C.java
@@ -0,0 +1,29 @@
+package C;
+
+public class C {
+
+	// ALWAYS SET TO TRUE
+	public static final boolean LEADERBOARDS = true;
+
+	// BANNER ID FROM ADMOB
+	public static String AD_UNIT_ID_BANNER = "ca-app-pub-5555715963974270/2007076944";
+
+	// INTERSTITIAL ID FROM ADMOB
+	public static String AD_UNIT_ID_INTERSTITIAL = "ca-app-pub-5555715963974270/3483810149";
+	// LEADERBOARD ID (you should also set the app_id in res>values>strings.xml)
+	public static String LEADERBOARD_ID = "CgkIvu2o8uAdEAIQAA";
+
+	// NAME YOUR GAME
+	public static CharSequence gameName = "Just Jump";
+
+	public static String centerCircleColor = "";
+	public static String secondCenterCircle = "FFFEFF"; // NEVER SET THIS ONE TO
+														// "" (empty)
+	public static String backgroundColor = "";
+	public static String boxColor = "";
+	public static String ballColor = "";
+
+	// RADIUS OF THE CENTER CIRCLE
+	public static final int radiusCenterCircle = 55;
+
+}
